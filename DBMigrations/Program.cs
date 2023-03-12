7046615d4e7c5707f6d8b41428a7aed92ef4844e
AveRageVXC <vladislav.osokin.2005@mail.ru>
2023-03-13 00:16:52 +0300

    fix: docker-compose

--- DBMigrations/Program.cs
+++ DBMigrations/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 
 var environmentName = Environment.GetCommandLineArgs()[1];
+environmentName = "Release";
 IConfigurationRoot? configuration = null;
 try
 {
